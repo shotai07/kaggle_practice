87cce6336fd0648fa255881b4ff5b110636566fd
shotai07 <shota.imafuku@gmail.com>
2022-04-13 21:43:22 +0900

    edit_dockerfile_for_jupyter

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM python:3.8.13-buster
 
 ENV PYTHONUNBUFFERED=1 \
     PYTHONDONTWRITEBYTECODE=1 \
-    PYTHONUTF8=1
+    PYTHONUTF8=1 \
+    TZ=Asia/Tokyo
 
 WORKDIR /app
 
--- jupyter.sh
+++ jupyter.sh
@@ -1,1 +1,1 @@
-jupyter notebook --port=8888 --ip=0.0.0.0 --allow-root --NotebookApp.token=''
+nohup jupyter lab --port=8888 --ip=0.0.0.0 --allow-root --NotebookApp.token='' >> jupyter.log 2>&1 &
